5055609f2d4b05ee50a0285d66099a40edb5d819
Mayank Bhardwaj <Mayank.2.Bhardwaj@Coforge.com>
2026-03-23 15:27:05 +0530

    Add day 21 code files

---
+++ Day21_OrderItemSpringBootPrj/src/main/java/com/coforge/Day21OrderItemSpringBootPrjApplication.java
@@ -0,0 +1,13 @@
+package com.coforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21OrderItemSpringBootPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21OrderItemSpringBootPrjApplication.class, args);
+	}
+
+}
